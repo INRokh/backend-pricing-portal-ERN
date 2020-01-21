9d0f2a741d4e9ec116fa740eca63670d165daee2
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-21 15:41:07 +1100

    Multer tutorial 1

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require('./routes'));
+//adding a reference path for the stati folder unloads in app.js  
+app.use("/uploads", express.static("uploads")); 
 
 module.exports = app;
