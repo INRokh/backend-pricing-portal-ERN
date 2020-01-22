fc4fff0e0d19a6103aba69974d16d772296d6154
Ellie <elliechen.etc@gmail.com>
2020-01-22 20:23:48 +1100

    Moved code from app.js to image & Changed endpoint to 'images/upload'

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const multer = require("multer");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require("./routes"));
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "reactUpload");
-  },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage }).single("file");
-
-app.post("/upload", function(req, res) {
-  upload(req, res, function(err) {
-    if (err instanceof multer.MulterError) {
-      return res.status(500).json(err);
-    } else if (err) {
-      return res.status(500).json(err);
-    }
-    return res.status(200).send(req.file);
-  });
-});
 
 module.exports = app;
--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,5 +1,27 @@
 const ImageModel = require("../database/models/image_model");
 
+//Upload image from React to Express
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploadFromReact");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+const upload = multer({ storage: storage }).single("file");
+function uploadfromReact(req, res) {
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
+}
+
 async function index(req, res) {
   const images = await ImageModel.find({ is_active: true }).catch(err =>
     res.status(500).send(err)
@@ -49,6 +71,6 @@ module.exports = {
   index,
   create,
   destroy,
-  update
-  //   upload
+  update,
+  uploadfromReact
 };
