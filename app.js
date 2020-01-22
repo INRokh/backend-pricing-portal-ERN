f62d266ebc4fa3558a230b092bbe95575b09084e
Ellie <elliechen.etc@gmail.com>
2020-01-22 17:07:45 +1100

    1. s3 show image, 2. upload-img-from-React-to-Express

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const multer = require("multer");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require("./routes"));
 
-// Importing AWSPresigner
-const { generateGetUrl, generatePutUrl } = require("./aws/AWSPresigner");
-
-// GET URL
-app.get("/generate-get-url", (req, res) => {
-  // Both Key and ContentType are defined in the client side.
-  // Key refers to the remote name of the file.
-  const { Key } = req.query;
-  generateGetUrl(Key)
-    .then(getURL => {
-      res.send(getURL);
-    })
-    .catch(err => {
-      res.send(err);
-    });
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "reactUpload");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
 });
 
-// PUT URL
-app.get("/generate-put-url", (req, res) => {
-  // Both Key and ContentType are defined in the client side.
-  // Key refers to the remote name of the file.
-  // ContentType refers to the MIME content type, in this case image/jpeg
-  const { Key, ContentType } = req.query;
-  generatePutUrl(Key, ContentType)
-    .then(putURL => {
-      res.send({ putURL });
-    })
-    .catch(err => {
-      res.send(err);
-    });
+const upload = multer({ storage: storage }).single("file");
+
+app.post("/upload", function(req, res) {
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
 });
 
 module.exports = app;
--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,44 +1,54 @@
-const ImageModel = require('../database/models/image_model')
+const ImageModel = require("../database/models/image_model");
 
 async function index(req, res) {
-    const images = await ImageModel.find({is_active: true})
-        .catch(err => res.status(500).send(err));
-    res.json(images);
+  const images = await ImageModel.find({ is_active: true }).catch(err =>
+    res.status(500).send(err)
+  );
+  res.json(images);
 }
 
 async function create(req, res) {
-    let imageObjs = []
-    for(let image of req.body.images){
-        imageObjs.push({title: image.title, url: image.url})
-    }
-        const images = await ImageModel.insertMany(imageObjs)
-            .catch(err => res.status(500).send(err));
-    res.json(images);
+  let imageObjs = [];
+  for (let image of req.body.images) {
+    imageObjs.push({ title: image.title, url: image.url });
+  }
+  const images = await ImageModel.insertMany(imageObjs).catch(err =>
+    res.status(500).send(err)
+  );
+  res.json(images);
 }
 
 async function destroy(req, res) {
-    console.log(req.body)
-    let deletedImages = []
-    for(let id of req.body.ids){
-        deletedImages.push(await ImageModel.findByIdAndUpdate(id, {is_active: false})
-            .catch(err => res.status(500).send(err)));
-    }
-    res.json(deletedImages);
+  console.log(req.body);
+  let deletedImages = [];
+  for (let id of req.body.ids) {
+    deletedImages.push(
+      await ImageModel.findByIdAndUpdate(id, { is_active: false }).catch(err =>
+        res.status(500).send(err)
+      )
+    );
+  }
+  res.json(deletedImages);
 }
 
 async function update(req, res) {
-    console.log(req.body)
-    let updatedImage = [];
-    for(let image of req.body.images){
-        updatedImage.push(await ImageModel.findByIdAndUpdate(image.id, {title: image.title, url: image.url})
-            .catch(err => res.status(500).send(err)));
-    }
-    res.json(updatedImage);
+  console.log(req.body);
+  let updatedImage = [];
+  for (let image of req.body.images) {
+    updatedImage.push(
+      await ImageModel.findByIdAndUpdate(image.id, {
+        title: image.title,
+        url: image.url
+      }).catch(err => res.status(500).send(err))
+    );
+  }
+  res.json(updatedImage);
 }
 
 module.exports = {
-    index,
-    create,
-    destroy,
-    update
-}
+  index,
+  create,
+  destroy,
+  update
+  //   upload
+};
