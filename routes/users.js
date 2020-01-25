2a47ca1b029e857c0927f0248d38881a3a55aba0
INRokh <ilnarnika@gmail.com>
2020-01-26 10:52:01 +1100

    Added auth routes.

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,29 @@ const { celebrate, Joi } = require('celebrate');
 
 // Joi API: https://github.com/hapijs/joi/blob/v14.3.1/API.md
 
+// Get list of all users 
+router.get('./',
+  passport.authenticate('jwt', { session : false }),
+  UsersController.index
+);
+
+// Create a new user
 router.post('/', celebrate({
-    body: { 
-        name: Joi.string().name().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().required(),
+  body: { 
+      username: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+  }
+  }), 
+  UsersController.register);
 
-    }
-}), UsersController.register);
-
-router.post('/login', 
+// Get jwt token using passport-local-mongoose strategy 
+router.post('/login', celebrate({
+  body: {
+    username: Joi.string().required(),
+    password: Joi.string().required()
+  }
+  }),
   passport.authenticate('local', { session : false }),
   UsersController.login
 );
